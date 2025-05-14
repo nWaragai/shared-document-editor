8a6fcb2c0a268836c2b2343ab0985d8d5aad56d4
nwara <waragai@infocity.co.jp>
2025-05-14 18:54:04 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      this is app page
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/EditorPage.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+
+export const EditorPage: FC = () => {
+  return(
+    <>
+      this is EditorPage
+    </>
+  )
+}
